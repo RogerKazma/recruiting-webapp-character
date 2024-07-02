602f9e95fc6f12a2c912757880288d098e98d7b2
Roger Kazma <roger.kazma@gmail.com>
2024-07-02 08:41:51 -0400

    Add modifiers

--- src/components/Attribute.js
+++ src/components/Attribute.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Row from "../common_components/Row";
 
+function computeModifier(value) {
+  return Math.floor((value - 10) / 2);
+}
+
 export default function Attribute({ name, value, onIncrement, onDecrement }) {
+  const modifier = computeModifier(value);
+
   return (
     <Row>
-      {`${name}: ${value}`}
+      {`${name}: ${value} (Modifier: ${modifier})`}
       <button name={name} onClick={onIncrement}>
         +
       </button>
